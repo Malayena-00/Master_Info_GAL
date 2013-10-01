09f4ee658cae9cee27bc803b2e9a1235132d0b9c
Username69 <quentoche@hotmail.com>
2013-10-01 14:11:03 +0200

    Update Yinsh.java
    Update du code 01/10 14h10

--- GAL_TP1/src/Yinsh.java
+++ GAL_TP1/src/Yinsh.java
@@ -1,51 +1,61 @@
-
 public class Yinsh {
 
-	Color[][] plateauYinsh = new Color[11][11];
+	Color[][] _plateauYinsh = new Color[11][11];
+	double _random = Math.random();
+	Color _lastplayed;
 
 	public Yinsh(){
-		for (int i = 0; i < plateauYinsh.length; i++){
-			for (int j = 0; j < plateauYinsh.length; j++){
-				plateauYinsh[i][j] = Color.NULL;
+		for (int i = 0; i < this._plateauYinsh.length; i++){
+			for (int j = 0; j < this._plateauYinsh.length; j++){
+				this._plateauYinsh[i][j] = Color.NULL;
 			}
 		}
-		
-		plateauYinsh[0][0] = Color.UNEXISTING;
-		plateauYinsh[0][5] = Color.UNEXISTING;
-		plateauYinsh[5][0] = Color.UNEXISTING;
-		plateauYinsh[5][11] = Color.UNEXISTING;
-		plateauYinsh[11][5] = Color.UNEXISTING;
-		plateauYinsh[11][11] = Color.UNEXISTING;
+
+		this._plateauYinsh[0][0] = Color.UNEXISTING;
+		this._plateauYinsh[0][5] = Color.UNEXISTING;
+		this._plateauYinsh[5][0] = Color.UNEXISTING;
+		this._plateauYinsh[5][10] = Color.UNEXISTING;
+		this._plateauYinsh[10][5] = Color.UNEXISTING;
+		this._plateauYinsh[10][10] = Color.UNEXISTING;
 	}
 
 	public enum Color{
-		BLACK, WHITE, NULL, UNEXISTING
+		BlackRing, WhiteRing, NULL, UNEXISTING
 	}
 
 	public Color current_color(){
-		double random = Math.random();
-		if(random <= 0.5){
-			return Color.WHITE;
+		if(this._random <= 0.5){
+			return Color.WhiteRing;
 		}else{
-			return Color.BLACK;
+			return Color.BlackRing;
 		}
 	}
 
 	public boolean put_ring(char coord_x, int coord_y, Color couleur){
-		plateauYinsh[coord_x-'A'][coord_y] = couleur;
-		return true;
+		if(this._plateauYinsh[coord_x-'A'][coord_y] == Color.NULL){
+			this._plateauYinsh[coord_x-'A'][coord_y] = couleur;
+			this._lastplayed = couleur;
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	public int CountRings(){
 		int nb = 0;
-		for (int i = 0; i < plateauYinsh.length; i++){
-			for (int j = 0; j < plateauYinsh.length; j++){
-				if (plateauYinsh[i][j] == Color.WHITE || plateauYinsh[i][j] == Color.BLACK){
+		for (int i = 0; i < this._plateauYinsh.length; i++){
+			for (int j = 0; j < this._plateauYinsh.length; j++){
+				if (this._plateauYinsh[i][j] == Color.WhiteRing || this._plateauYinsh[i][j] == Color.BlackRing){
 					nb++;
 				}
 			}
 		}
-		
+
 		return nb;
 	}
+
+	/*public static void main(String[] args) {
+		Yinsh a = new Yinsh();
+		System.out.print(a.plateauYinsh.length);
+	}*/
 }
